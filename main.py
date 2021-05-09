f7622874e8fbd9926e5120aef5b2ba9d9a28fa80
HyeongseokJeon 5900x <521hsgugu@kaist.ac.kr>
2021-05-09 22:28:24 +0900

    asdf

--- main.py
+++ main.py
@@ -31,9 +31,28 @@ class MainDialog(QMainWindow, _uiFiles.gui.Ui_Dialog):
         self.loadModel.clicked.connect(self.model_load)
         self.loadData.clicked.connect(self.data_load)
         self.next_idx.clicked.connect(self.next)
+        self.ego_path_enable.clicked.connect(self.visualization)
+        self.ego_path_aug_1.clicked.connect(self.visualization)
+        self.ego_path_aug_2.clicked.connect(self.visualization)
+        self.ego_path_aug_3.clicked.connect(self.visualization)
+        self.ego_path_aug_4.clicked.connect(self.visualization)
+        self.ego_path_aug_5.clicked.connect(self.visualization)
+        self.ego_path_aug_6.clicked.connect(self.visualization)
+        self.ego_path_aug_7.clicked.connect(self.visualization)
+        self.ego_path_aug_8.clicked.connect(self.visualization)
+
         self.idx = 0
         self.fov = 25
         self.data_dir = '/home/jhs/Desktop/SRFNet/LaneGCN/dataset/val/data/'
+        self.cand_toggles = [self.ego_path_enable,
+                             self.ego_path_aug_1,
+                             self.ego_path_aug_2,
+                             self.ego_path_aug_3,
+                             self.ego_path_aug_4,
+                             self.ego_path_aug_5,
+                             self.ego_path_aug_6,
+                             self.ego_path_aug_7,
+                             self.ego_path_aug_8]
 
     def next(self):
         self.cur_data = next(iter(self.data_loader))
@@ -43,13 +62,29 @@ class MainDialog(QMainWindow, _uiFiles.gui.Ui_Dialog):
         self.update_data()
 
     def update_data(self):
-        self.pred_plot.canvas.ax.clear()
         self.map_data.setText(self.cur_data['city'][0])
         self.num_of_vehicles_data.setText(str(self.cur_data['feats'][0].shape[0]))
         self.SceneInfo_data.setText('None')
         self.idx_data.setText(str(self.cur_data['idx'][0]))
-        self.pred_out = self.net(self.cur_data)
-        ade1, fde1, ade6, fde6 = self.get_eval_data(self.pred_out)
+        ego_aug = self.cur_data['ego_aug'][0]['traj']
+        self.ego_path_enable.setEnabled(True)
+        self.ego_path_enable.setChecked(True)
+        self.pred_out = []
+        with torch.no_grad():
+            self.pred_out.append(self.net(self.cur_data))
+        for i in range(8):
+            if i < ego_aug.shape[0]:
+                self.cand_toggles[i+1].setEnabled(True)
+                self.cand_toggles[i + 1].setChecked(True)
+                with torch.no_grad():
+                    data_tmp = self.cur_data.copy()
+                    data_tmp['action'][0][0:1, 0, :, :] = data_tmp['action'][0][0:1, i, :, :]
+                    self.pred_out.append(self.net(data_tmp))
+            else:
+                self.cand_toggles[i+1].setEnabled(False)
+                self.cand_toggles[i + 1].setChecked(False)
+
+        ade1, fde1, ade6, fde6 = self.get_eval_data(self.pred_out[0])
         self.ade1_data.setText(str(ade1)[:5])
         self.fde1_data.setText(str(fde1)[:5])
         self.ade6_data.setText(str(ade6)[:5])
@@ -57,6 +92,7 @@ class MainDialog(QMainWindow, _uiFiles.gui.Ui_Dialog):
         self.visualization()
 
     def visualization(self):
+        self.pred_plot.canvas.ax.clear()
         ego_cur_pos = self.cur_data['gt_preds'][0][0,0,:]
         xmin = ego_cur_pos[0] - self.fov
         xmax = ego_cur_pos[0] + self.fov
@@ -72,27 +108,41 @@ class MainDialog(QMainWindow, _uiFiles.gui.Ui_Dialog):
             for row in spamreader:
                 raw_data.append(row)
         raw_data = raw_data[1:]
-        time = np.asarray([float(raw_data[i][0].split(',')[0]) for i in range(len(raw_data))])
         x = np.asarray([float(raw_data[i][0].split(',')[3]) for i in range(len(raw_data))])
         y = np.asarray([float(raw_data[i][0].split(',')[4]) for i in range(len(raw_data))])
         veh_class = [raw_data[i][0].split(',')[2] for i in range(len(raw_data))]
         ego_index = [i for i,x in enumerate(veh_class) if x == 'AV']
         ego_x = x[ego_index]
         ego_y = y[ego_index]
+        ego_hist_x = ego_x[:20]
+        ego_hist_y = ego_y[:20]
+        ego_fut_x = ego_x[19:]
+        ego_fut_y = ego_y[19:]
         target_index = [i for i,x in enumerate(veh_class) if x == 'AGENT']
         target_x = x[target_index]
         target_y = y[target_index]
-
-        ego_aug = self.cur_data['ego_aug'][0]['traj']
-
-        for i in range(ego_aug.shape[0]):
-            aug_x = ego_aug[i,:,0]
-            aug_y = ego_aug[i,:,1]
-            self.pred_plot.canvas.ax.plot(aug_x, aug_y, '--', color='red')
-
-        self.pred_plot.canvas.ax.plot(ego_x, ego_y, '-', color='red')
-        self.pred_plot.canvas.ax.scatter(ego_x[-1], ego_y[-1], color='red')
+        self.pred_plot.canvas.ax.plot(ego_hist_x, ego_hist_y, '-', color='red')
         self.pred_plot.canvas.ax.plot(target_x, target_y, '-', color='blue')
+
+        ego_aug = self.cur_data['ego_aug'][0]['traj'].numpy().copy()
+        ego_aug = np.concatenate([ego_aug, np.zeros_like(ego_aug[:,0:1,:])], axis=1)
+        for i in range(ego_aug.shape[0]):
+            ego_aug[i,:,:] = np.concatenate([np.expand_dims(np.asarray([ego_hist_x[-1], ego_hist_y[-1]]), axis=0), ego_aug[i,:30,:]], axis=0)
+            if self.cand_toggles[i+1].isChecked():
+                aug_x = ego_aug[i,:,0]
+                aug_y = ego_aug[i,:,1]
+                self.pred_plot.canvas.ax.plot(aug_x, aug_y, '--', color='red')
+                pred_out = self.pred_out[i+1]
+                best_idx = np.argmax(pred_out['cls'][0][0].cpu().detach().numpy())
+                pred_reg = pred_out['reg'][0][0, best_idx, :, :]
+                self.pred_plot.canvas.ax.plot(pred_reg[:,0].cpu(), pred_reg[:,1].cpu(), '--', color='blue')
+        if self.ego_path_enable.isChecked():
+            self.pred_plot.canvas.ax.plot(ego_fut_x, ego_fut_y, '-', color='red')
+            self.pred_plot.canvas.ax.scatter(ego_x[-1], ego_y[-1], color='red')
+            pred_out = self.pred_out[0]
+            best_idx = np.argmax(pred_out['cls'][0][0].cpu().detach().numpy())
+            pred_reg = pred_out['reg'][0][0, best_idx, :, :]
+            self.pred_plot.canvas.ax.plot(pred_reg[:, 0].cpu(), pred_reg[:, 1].cpu(), '--', color='blue')
         self.pred_plot.canvas.ax.scatter(target_x[-1], target_y[-1],color='blue')
         self.pred_plot.canvas.draw()
         self.pred_plot.canvas.ax.axis('equal')
@@ -131,7 +181,7 @@ class MainDialog(QMainWindow, _uiFiles.gui.Ui_Dialog):
                                       drop_last=True,
                                       )
 
-        self.dataInfo.setText(os.path.join(self.root_dir, '../SRFNet/SRFNet/dataset/preprocess_GAN/val_crs_dist6_angle90.p'))
+        self.dataInfo.setText(config['preprocess_val'])
         self.cur_data = next(iter(self.data_loader))
         self.update_data()
 
@@ -145,9 +195,21 @@ class MainDialog(QMainWindow, _uiFiles.gui.Ui_Dialog):
 
         model_id = os.path.split((os.path.dirname(weight_file_dir)))[-1]
         sys.path.extend([os.path.join(os.path.dirname(weight_file_dir), 'files')])
-
         if model_id == 'SSL_baseline_end_to_end_supervised_learning':
             model = import_module('SSL_baseline_train')
+            parser = model.parser
+            args = parser.parse_args()
+            config, config_enc, Dataset, collate_fn, net, loss, opt, post_process = model.model.get_model(args)
+            net.load_state_dict(weight_file['state_dict'])
+            self.args = args
+            self.Dataset = Dataset
+            self.net = net
+            self.collate_fn = collate_fn
+            self.post_process = post_process
+            self.loss = loss
+
+        if model_id == 'SSL_downstream_initialize_and_freeze_backbone_and_encoder':
+            model = import_module('SSL_downstream_train')
             parser = model.parser
             args = parser.parse_args()
             config, config_enc, Dataset, collate_fn, net, loss, opt, post_process = model.model.get_model(args)
